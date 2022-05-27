931b44b05b2b5c299aa8c9986b46202143e35023
Mohammad Al-Thunayan <malthunayan4@gmail.com>
2022-05-27 17:47:38 +0300

    Fix meta data

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_1.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "airport.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
